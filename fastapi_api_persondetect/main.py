590bd1517809a5339f10f167b18635723bcb4f95
Igor Lucena <igorlpa90@gmail.com>
2024-08-16 15:29:51 -0300

    final do projeto fasapi

--- fastapi_api_persondetect/main.py
+++ fastapi_api_persondetect/main.py
@@ -3,10 +3,13 @@ from fastapi import FastAPI
 from routes import detection, home, login
 
 
-app = FastAPI()
-app.include_router(detection.router)
-app.include_router(home.router)
-app.include_router(login.router)
+app = FastAPI(
+    title='Exemplo detector de pessoa com FastAPI',
+    version='0.0.1'
+    )
+app.include_router(detection.router, tags=['detection'])
+app.include_router(home.router, tags=['login'])
+app.include_router(login.router, tags=['login'])
 
 
 @app.get("/")
